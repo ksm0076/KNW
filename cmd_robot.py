213916c3b7007f4626d4d386d01b72b72baa21fd
ksm0076 <60387895+ksm0076@users.noreply.github.com>
2024-11-19 14:32:21 +0900

    one print

--- cmd_robot.py
+++ cmd_robot.py
@@ -31,24 +31,25 @@ class RobotController:
         self.current_pose = Pose()
         self.pallet_axis = Quaternion()
         
-        self.kp_linear = 0.4
-        self.kp_angular = 2
+        self.kp_linear = 0.2
+        self.kp_angular = 1
 
-        self.max_linear_speed = 2
+        self.max_linear_speed = 0.75
         self.max_angular_speed = 2.0
 
         self.last_detection_time = time.time()
         self.searching = False
         self.rotation_direction = 1  # 1 for clockwise, -1 for counter-clockwise
-       
-        
+
+        self.last_status = None  # 추가: 마지막 상태를 저장
+
     def pallet_callback(self, msg):
         self.pallet_axis = msg
         distance = self.calculate_distance()
         self.last_detection_time = time.time()  # Update last detection time when detected
         self.searching = False  # Stop searching if person is detected
 
-        if distance > 1.5:
+        if distance > 1:
             print("Following person")
             cmd_vel_msg = Twist()
             yaw_error = self.calculate_yaw_error()
@@ -58,7 +59,6 @@ class RobotController:
             linear_speed = self.limit_speed(linear_speed, self.max_linear_speed)
             angular_speed = self.limit_speed(angular_speed, self.max_angular_speed)
 
-            print(f"Distance: {distance}, speed: {linear_speed}")
             cmd_vel_msg.linear.x = linear_speed
             cmd_vel_msg.angular.z = angular_speed
             
@@ -68,12 +68,16 @@ class RobotController:
                 self.rotation_direction = -1
             
             self.cmdvel_pub.publish(cmd_vel_msg)
-        elif distance <= 1.5 and distance > 0:
+        elif distance <= 1 and distance > 0:
             print("STOP")
             self.stop_robot()
 
     def status_callback(self, msg):
-        print("status_callback :", msg)
+        # 상태가 변경되었을 때만 출력
+        if msg.data != self.last_status:
+            print("status_callback :", msg)
+            self.last_status = msg.data  # 현재 상태를 업데이트
+
         if msg.data == "lost":
             current_time = time.time()
             if current_time - self.last_detection_time > 3:
